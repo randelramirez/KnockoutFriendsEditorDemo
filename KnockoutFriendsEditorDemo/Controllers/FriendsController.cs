12d5107cdcfabebe6287db41ef276b2db2d513da
randelramirez <randelramirez1@gmail.com>
2016-03-25 02:32:51 +0800

    initial commit

---
+++ KnockoutFriendsEditorDemo/Controllers/FriendsController.cs
@@ -0,0 +1,28 @@
+﻿using KnockoutFriendsEditorDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnockoutFriendsEditorDemo.Controllers
+{
+    public class FriendsController : Controller
+    {
+        // GET: Friends
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Save(Person person)
+        {
+            // Just to show we have actually got the data as .NET objects
+            var message = string.Format("Saved {0} {1}", person.FirstName, person.LastName);
+            message += string.Format(" with {0} friends", person.Friends.Count);
+            message += string.Format(" ({0} on Twitter)", person.Friends.Where(x => x.IsOnTwitter).Count());
+
+            return Json(new { message });
+        }
+    }
+}
---
+++ KnockoutFriendsEditorDemo/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnockoutFriendsEditorDemo.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Friend> Friends { get; set; }
+    }
+
+}
